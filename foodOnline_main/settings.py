c488dff83f4e833bc39e6283951a3809043724f8
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-05-22 04:20:32 +0500

    🧑‍💻 feat: create custom user model

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -74,7 +76,7 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+#settings
 DATABASES = {
      'default': {
        'ENGINE': 'django.db.backends.postgresql',
@@ -86,6 +88,7 @@ DATABASES = {
 
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

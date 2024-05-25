0ae91d43ac181c54ceea7f73b0aab181ddc7f8aa
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-05-26 03:13:07 +0500

    🔒 fix: hash password from form

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,21 @@ from .models import User
 def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
-        if form.is_valid:
-            user = form.save(commit=False)
+        if form.is_valid():
+            # Create the user using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            
+            # Create user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name,last_name=last_name, username=username,email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
             return redirect('register_user')

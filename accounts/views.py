b2bcb76bb1d1b95fcefe4833812fa59385f3b2cb
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-06-01 22:13:08 +0500

    🔐 feat: implement login and logout features

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
-from django.contrib import messages
+from django.contrib import messages, auth
 # Create your views here.
 
 def registerUser(request):
@@ -72,10 +72,25 @@ def registerVendor(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are logged in")
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    messages.info(request, "You are logged out")
+    return redirect('login')
+    
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')

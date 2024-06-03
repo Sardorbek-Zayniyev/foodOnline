67c4c48e18cfeb920b472f790019609dbc12b48c
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-06-03 07:42:14 +0500

    🎨 feat: set up restaurant profile form

--- vendor/forms.py
+++ vendor/forms.py
@@ -3,7 +3,7 @@ from .models import Vendor
 
 
 class VendorForm(forms.ModelForm):
-    # vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    vendor_license = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license'] 

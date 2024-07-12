47a38718efce9bc4961589dd92873fd7065b8794
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-07-13 04:20:46 +0500

    🗂️ feat: create cart model

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from marketplace.models import Cart
+
 # Register your models here.
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = {'user', 'fooditem', 'quantity', 'update_at'}
+
+admin.site.register(Cart)
+

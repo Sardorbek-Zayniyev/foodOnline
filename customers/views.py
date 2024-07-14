f2caf4b9d5c8657ae5dc5fda649ddd19bc2cb181
Sardorbek-Zayniyev <me.zayniyev@gmail.com>
2024-07-15 04:18:57 +0500

    👤 feat: update user info features from frontend

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+
+from accounts.forms import UserInfoForm, UserProfileForm
+from accounts.models import UserProfile
+from accounts.utils import check_role_customer
 
 # Create your views here.
 
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def customer_profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, 'Profile updated')
+            return redirect('customer_profile')
+        else:
+            print(profile_form.errors)
+            print(user_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
 
-def customer_profile (requset):
-    return render(requset, 'customers/customer_profile.html')
+    context = {
+        'profile_form': profile_form,
+        'user_form' : user_form,
+        'profile': profile,
+    }
+    return render(request, 'customers/customer_profile.html', context)
